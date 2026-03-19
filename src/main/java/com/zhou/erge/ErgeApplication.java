f60469a3aadce025094af446dfa05afb751d7a73
we1-230 <3153309115@qq.com>
2026-03-19 11:50:16 +0800

    初始化

---
+++ src/main/java/com/zhou/erge/ErgeApplication.java
@@ -0,0 +1,13 @@
+package com.zhou.erge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErgeApplication.class, args);
+    }
+
+}
