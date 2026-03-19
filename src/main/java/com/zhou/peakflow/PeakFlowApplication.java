93ee160ad68d33d7fc9ac29b2a021d0376bb0b29
we1-230 <3153309115@qq.com>
2026-03-19 10:46:55 +0800

    初始化项目

---
+++ src/main/java/com/zhou/peakflow/PeakFlowApplication.java
@@ -0,0 +1,13 @@
+package com.zhou.peakflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeakFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeakFlowApplication.class, args);
+    }
+
+}
---
+++ src/main/java/e.java
@@ -0,0 +1,5 @@
+public class e {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
